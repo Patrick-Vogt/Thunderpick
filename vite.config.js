9d0101320a3d1f188a2cea872ba769f11c7ecf36
Patrick <mail@patrick-vogt.com>
2026-05-22 10:46:49 +0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/thunderpick/', // Replace 'thunderpick' with your repo name
+  base: '/Thunderpick/',
 })
